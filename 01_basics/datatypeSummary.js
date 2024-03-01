ed26bda04688de2563434b4551cf75fdbc869be1
Suryansh Pandey <suryanshp1@gmail.com>
2024-03-01 20:29:01 +0000

    feat: datatype summary

---
+++ 01_basics/datatypeSummary.js
@@ -0,0 +1,32 @@
+// Primitive
+
+// 7 type : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Non - Primitive - (Reference) : Array, Objects, Functions
+
+const score = 100
+const marks = 23.5
+
+const IsLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+// const bigNumber = 74974974994794723973n
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+// console.log(id == anotherId);
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "Surya",
+    age: 24
+}
+
+
+const myFunction = function() {
+    console.log("Hello World");
+}
+console.log(typeof outSideTemp);
+console.log(typeof myFunction);
