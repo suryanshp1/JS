44c932ee3942bd0f6b7855eb2be71c9e2ad8734e
Suryansh Pandey <suryanshp1@gmail.com>
2024-03-01 19:45:00 +0000

    feat: operators

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,37 @@
+let score = "Surya";
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => nan
+// true -> 1, false->0
+let IsLoggedIn = "wdw";
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//  "" => false
+// "hitesh" => true
+
+let value=3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2**2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(+true); //1 
+// console.log(+""); //0
+
+let gameCounter = 100
+// gameCounter++
+// console.log(gameCounter);
+// ++gameCounter
+// console.log(gameCounter);
+console.log(++gameCounter);
